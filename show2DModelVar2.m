a9aabe8c8d4a19eb855eafbdc2126c0cae7fcc53
Gabriel Santiago Pujol Fariña <gfarina@TARDOS.ATD-LAB>
2016-08-31 17:39:04 -0300

    Updated the functionality of my versions of the original functions
    - show2DModelVar2 minimum changes, only readability
    - show2DModelVar2_Gabriel_Separated    a LOT of things, it plots
    beautifully now, size-independent
    - show2DModelVar3_Gabriel_Separated    a LOT of things, it plots
    beautifully now, size-independent
    - test.m     adapted it a bit, in order to test my stuff

--- show2DModelVar2.m
+++ show2DModelVar2.m
@@ -27,36 +27,64 @@ model_CCSIM=re2;
 model_CCSIM(:,:,51)=out;
 
 
-
-
 S=30*ones(1,51); S(51)=120;
+
 
 abc=[1,2,3,4,6];
 for ii=1:1										%1:3
 	i=abc(ii);
 	
+	
+	
+	
 	figure;
-	ddd=squeeze(JS_dispat(i,:,:));
-	[Y1_dispat,e_dispat] = cmdscale(double(ddd));
-	x_MR=Y1_dispat(:,1); y_MR=Y1_dispat(:,2); z_MR=Y1_dispat(:,3);
-	%%
-	x_MR=x_MR-x_MR(51); y_MR=y_MR-y_MR(51); z_MR=z_MR-z_MR(51);
-	%%
-	[d1,IX1] = sort(ddd(51,:));
 	scatter3(0,0,0, S(51), 'k','filled');
 	hold on;
-	%%
+		
+	%% first set of points
+	
+	ddd=squeeze(JS_dispat(i,:,:));
+	
+	% get the new Coordinates for the n points in a new p-dimensional space
+	% along with the EigenValues (first k are positive if matrix euclidean)
+	[Y1_dispat,e_dispat] = cmdscale(double(ddd));
+	
+	% get the new coordinates for every point, for plotting reasons...
+	x_MR=Y1_dispat(:,1); 
+	y_MR=Y1_dispat(:,2); 
+	z_MR=Y1_dispat(:,3);
+	% centering
+	x_MR=x_MR-x_MR(51); 
+	y_MR=y_MR-y_MR(51); 
+	z_MR=z_MR-z_MR(51);
+	
+	% sorting
+	[d1,IX1] = sort(ddd(51,:));
+	% plotting
 	scatter3(x_MR,y_MR,z_MR, S, 'g','filled');
+		
+	%% second set of points
 	
 	ddd=squeeze(JS_CCSIM(i,:,:));
-	[Y1_CCSIM,e_CCSIM] = cmdscale(double(ddd));
-	x_CCSIM=Y1_CCSIM(:,1); y_CCSIM=Y1_CCSIM(:,2); z_CCSIM=Y1_CCSIM(:,3);
-	%%
-	x_CCSIM=x_CCSIM-x_CCSIM(51); y_CCSIM=y_CCSIM-y_CCSIM(51); z_CCSIM=z_CCSIM-z_CCSIM(51);
-	[d2,IX2] = sort(ddd(51,:));
 	
+	
+	[Y1_CCSIM,e_CCSIM] = cmdscale(double(ddd));
+	
+	% get the new coordinates for every point, for plotting reasons...
+	x_CCSIM=Y1_CCSIM(:,1); 
+	y_CCSIM=Y1_CCSIM(:,2); 
+	z_CCSIM=Y1_CCSIM(:,3);
+	% centering
+	x_CCSIM=x_CCSIM-x_CCSIM(51); 
+	y_CCSIM=y_CCSIM-y_CCSIM(51); 
+	z_CCSIM=z_CCSIM-z_CCSIM(51);
+	
+	% sorting
+	[d2,IX2] = sort(ddd(51,:));
+	% plotting
 	scatter3(x_CCSIM,y_CCSIM,z_CCSIM, S,'b','filled');
 		
+	%% third set of points
 %	ddd=squeeze(JS_sisim(i,:,:));
 %	[Y1_sisim,e_sisim] = cmdscale(double(squeeze(JS_sisim(i,:,:))));
 %	x_sisim=Y1_sisim(:,1); y_sisim=Y1_sisim(:,2); z_sisim=Y1_sisim(:,3);
@@ -66,7 +94,16 @@ for ii=1:1										%1:3
 %	S(IX3(2))=60; S(IX3(21))=60; S(IX3(31))=60; S(IX3(41))=60; S(IX3(51))=60;
 %	scatter3(x_sisim,y_sisim,z_sisim,S,'r','filled');
 
+	
 
+
+
+
+
+
+
+
+	%% putting the labels to the special points
 	text(x_MR(IX1( 2)),y_MR(IX1( 2)),z_MR(IX1( 2)),'\leftarrow 1','FontSize',10,'Color','g');
 	text(x_MR(IX1(21)),y_MR(IX1(21)),z_MR(IX1(21)),'\leftarrow 20','FontSize',10,'Color','g');
 	text(x_MR(IX1(31)),y_MR(IX1(31)),z_MR(IX1(31)),'\leftarrow 30','FontSize',10,'Color','g');
@@ -85,22 +122,28 @@ for ii=1:1										%1:3
 %	text(x_sisim(IX3(41)),y_sisim(IX3(41)),z_sisim(IX3(41)),'\rightarrow 40','FontSize',10,'Color','r');
 %	text(x_sisim(IX3(51)),y_sisim(IX3(51)),z_sisim(IX3(51)),'\rightarrow 50','FontSize',10,'Color','r');
 	
-	
+	%% finishing the first, 3-D, plot, Plotting the legend
 	scatter3(0,0,0,S(51),'k','filled');
 	legend('Training image','DISPAT','CCSIM');
 	title(['Multi Resolution= ' int2str(i)]);
 	hold off;
+	
+	
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	
+	%% XY plane!!!
 	figure;
 	S=20*ones(1,51); S(51)=100;
 	S(IX1(2))=30; S(IX1(21))=30; S(IX1(31))=30; S(IX1(41))=30; S(IX1(51))=30;
 	scatter(x_MR,y_MR,S,'g','filled');
+	hold on;
+	
 	text(x_MR(IX1( 2)),y_MR(IX1( 2)),'\leftarrow  1','FontSize',10,'Color','g');
 	text(x_MR(IX1(21)),y_MR(IX1(21)),'\leftarrow 20','FontSize',10,'Color','g');
 	text(x_MR(IX1(31)),y_MR(IX1(31)),'\leftarrow 30','FontSize',10,'Color','g');
 	text(x_MR(IX1(41)),y_MR(IX1(41)),'\leftarrow 40','FontSize',10,'Color','g');
 	text(x_MR(IX1(51)),y_MR(IX1(51)),'\leftarrow 50','FontSize',10,'Color','g');
-	hold on;
+	
 	
 	S=20*ones(1,51); S(51)=100;
 	S(IX2(2))=30; S(IX2(21))=30; S(IX2(31))=30; S(IX2(41))=30; S(IX2(51))=30;
@@ -134,7 +177,7 @@ out_c=model_dispat(:,:,IX1(1));
 
 
 
-
+	%% the closest guys, of the first set of points.
 figure;
 subplot(2,3,1)
 imshow(out1);
@@ -161,7 +204,8 @@ out_c=model_dispat(:,:,IX1(11+10*kk));
 	title([int2str(11+10*kk-1) 'th closest to Ti'] );
 end
 
-%%
+
+	%% the closest guys, of the second set of points.
 figure;
 subplot(2,3,1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -192,7 +236,9 @@ out_c=model_CCSIM(:,:,IX2(11+10*kk));
 	imshow(out1);
 	title([int2str(11+10*kk-1) 'th closest to Ti'] );
 end
-%%
+
+
+	%% the closest guys, of the third set of points.
 % figure;
 % subplot(2,3,1)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -225,6 +271,8 @@ end
 % end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+	%% XZ plane!
 figure;
 S=20*ones(1,51); S(51)=100;
 	S(IX1(2))=30; S(IX1(21))=30; S(IX1(31))=30; S(IX1(41))=30; S(IX1(51))=30;
@@ -258,6 +306,7 @@ hold off;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+	%% YZ plane!!!
 	figure;
 	S=20*ones(1,51); S(51)=100;
 	S(IX1(2))=30; S(IX1(21))=30; S(IX1(31))=30; S(IX1(41))=30; S(IX1(51))=30;
@@ -291,6 +340,14 @@ title(['y-z Plot & Multi Scale = ' int2str(i)]);
 legend('DISPAT','CCSIM','Training Image');
 hold off; 
 
+
+	%% the last figure:
+	% it plots a cumulative sum of eigenvalues to show the degree of 
+	% representativity of the space in respect to the amount of dimensions
+	% taken into account. (e.g., the 'test' example, for the DISPAT, we get
+	% a cummulative representativity of 0.699 with 3 dimensions, while 
+	% with the CCSIM we get 0.7495).
+	
 size_dispatE=size(Y1_dispat,2);
 size_CCSIME=size(Y1_CCSIM,2);
 % size_sisimE=size(Y1_sisim,2);
--- show2DModelVar2_Gabriel_Separated.m
+++ show2DModelVar2_Gabriel_Separated.m
@@ -1,58 +1,53 @@
-function show2DModelVar2_Gabriel_Separated(DisMtrx1, DisMtrx2, TrainigImage, re1, re2, NomTI, NomAlg1, NomAlg2)
+function show2DModelVar2_Gabriel_Separated(DisMtrx1, DisMtrx2, TrainigImage, realizations1, realizations2, NomTI, NomAlg1, NomAlg2)
 % this function is used for 2D case
 
 % show the variability of the two sets of realizations
 % input: DisMtrx1 is the distance matrix for Algorithm1 realizations
 % input: DisMtrx2 is the distance matrix for Algorithm2 realizations
 
-% input: TrainigImage is the training image:	e.g. 101*101
-% input: re1 is Algorithm1 realizations: 		e.g. 101*101*50
-% input: re2 is Algorithm2 realizations:		e.g. 101*101*50
+% input: TrainigImage is the training image:			e.g. 101*101
+% input: realizations1 is Algorithm1 realizations: 		e.g. 101*101*50
+% input: realizations2 is Algorithm2 realizations:		e.g. 101*101*50
 
-
-% NomAlg1 = 'Algorithm1';
-% NomAlg2 = 'Algorithm2';
 
 
 % the amount of realizations from each algorithm.
 num_re1 = size(DisMtrx1, 3);
 num_re2 = size(DisMtrx2, 3);
-
+% num_re3 = size(DisMtrx3, 3);
 
 % The scales of the points to be plotted for each algorithm.
 standardScale = 20;	mediumScale = 30;	 bigScale = 100;
-S1 = standardScale*ones(1, num_re1); S(num_re1) = bigScale;
-S2 = standardScale*ones(1, num_re2); S(num_re2) = bigScale;
+S1 = standardScale*ones(1, num_re1); S1(num_re1) = bigScale; % last is TI
+S2 = standardScale*ones(1, num_re2); S2(num_re2) = bigScale; % last is TI
+% S3 = standardScale*ones(1, num_re3+1); S3(num_re3+1) = bigScale; % last is TI
 
-	
+
 %% Including the Training Image into the collections of Realizations
-% realizations1 = re1;
-% realizations1(:, :, num_re1) = TrainigImage;
-% realizations2 = re2;
-% realizations2(:, :, num_re2) = TrainigImage;
+realizations1(:, :, num_re1) = TrainigImage;
+realizations2(:, :, num_re2) = TrainigImage;
+% realizations3(:, :, num_re3) = TrainigImage;
 
-
-
-%% Pyramid is the maximum amount of resolutions that will be worked.
-Pyramid = 1;
+%% Set of resolutions that will be worked.
 resolutions = [1, 2, 3, 4, 6];
 
-for ii = 1:Pyramid
+for ii = 1:1		% size(resolutions, 2);
 	%% starting the plot, for each resolution:
-	current_resolution=resolutions(ii);
+	current_resolution = resolutions(ii);
+	
 	% Here they do a 3-D Scatter Plot. Let's decypher it:
 	% First, start the 'figure' window, and then plot the axis...
 	figure('Name', 'Tridimensional plot', 'NumberTitle', 'off');
-	scatter3(0, 0, 0, S(num_re1), 'k', 'filled');
+	scatter3(0, 0, 0, bigScale, 'k', 'filled');
 	hold on;
 	
 	%% WORKING WITH THE FIRST DISTANCE MATRIX!!!
 	% set ddd to the DisMatrix corresponding to the current resolution
-	ddd=squeeze(DisMtrx1(current_resolution, :, :));
+	ddd = squeeze(DisMtrx1(current_resolution, :, :));
 		
 	% get the new Coordinates for the n points in a new p-dimensional space
 	% along with the EigenValues (first k are positive if matrix euclidean)
-	[Y1_re1, ~] = cmdscale(double(ddd)); % e_re1
+	[Y1_re1, e_re1] = cmdscale(double(ddd));	% e_re1
 	
 	% get the new coordinates for every point, for plotting reasons...
 	x_re1 = Y1_re1(:, 1);
@@ -63,17 +58,17 @@ for ii = 1:Pyramid
 	y_re1 = y_re1 - y_re1(num_re1);
 	z_re1 = z_re1 - z_re1(num_re1);
 	
-	% Positions of the 'sorted' list, according to the distances to the TI
+	% Sorting
 	[~, IX1] = sort(ddd(num_re1, :));
-	
+	% Plotting the set of points
 	scatter3(x_re1, y_re1, z_re1, S1, 'g', 'filled');		% FIRST DATA PLOTTED!!!
 	
 	%% WORKING WITH THE SECOND DISTANCE MATRIX!!!
 	% set ddd to the DisMatrix corresponding to the current resolution
-	ddd=squeeze(DisMtrx2(current_resolution, :, :));
+	ddd = squeeze(DisMtrx2(current_resolution, :, :));
 	% get the new Coordinates for the n points in a new p-dimensional space
 	% along with the EigenValues (first k are positive if matrix euclidean)
-	[Y1_re2, ~] = cmdscale(double(ddd)); % e_re2
+	[Y1_re2, e_re2] = cmdscale(double(ddd));	% e_re2
 	
 	% get the new coordinates for every point, for plotting reasons...
 	x_re2 = Y1_re2(:, 1);
@@ -86,90 +81,157 @@ for ii = 1:Pyramid
 
 	% Positions of the 'sorted' list, according to the distances to the TI
 	[~, IX2] = sort(ddd(num_re2, :));
-	
+	% Plotting
 	scatter3(x_re2, y_re2, z_re2, S2, 'b', 'filled');	% SECOND DATA PLOTTED!!!
 	
+	%% WORKING WITH THE THIRD DISTANCE MATRIX!!!
+	% set ddd to the DisMatrix corresponding to the current resolution
+	ddd = squeeze(DisMtrx3(current_resolution, :, :));
+	% get the new Coordinates for the n points in a new p-dimensional space
+	% along with the EigenValues (first k are positive if matrix euclidean)
+	[Y1_re3, e_re3] = cmdscale(double(ddd));
 	
+	% get the new coordinates for every point, for plotting reasons...
+	x_re3 = Y1_re3(:, 1);
+	y_re3 = Y1_re3(:, 2);
+	z_re3 = Y1_re3(:, 3);
+	% Centering the coordinates around the Training Image:
+	x_re3 = x_re3 - x_re3(num_re1);
+	y_re3 = y_re3 - y_re3(num_re1);
+	z_re3 = z_re3 - z_re3(num_re1);
 	
+	% Positions of the 'sorted' list, according to the distances to the TI
+	[~, IX3] = sort(ddd(num_re3, :));
+	% Plotting
+	scatter3(x_re3, y_re3, z_re3, S3, 'r', 'filled');	% THIRD DATA PLOTTED!!!
 	
+	%% putting the labels to the special points
+	text(x_re1(IX1( 2)),y_re1(IX1( 2)),z_re1(IX1( 2)),'\rightarrow  1','FontSize',10,'Color','g');
+	text(x_re1(IX1(21)),y_re1(IX1(21)),z_re1(IX1(21)),'\rightarrow 20','FontSize',10,'Color','g');
+	text(x_re1(IX1(31)),y_re1(IX1(31)),z_re1(IX1(31)),'\rightarrow 30','FontSize',10,'Color','g');
+	text(x_re1(IX1(41)),y_re1(IX1(41)),z_re1(IX1(41)),'\rightarrow 40','FontSize',10,'Color','g');
+	text(x_re1(IX1(51)),y_re1(IX1(51)),z_re1(IX1(51)),'\rightarrow 50','FontSize',10,'Color','g');
 	
+	text(x_re2(IX2( 2)),y_re2(IX2( 2)),z_re2(IX2( 2)),'\rightarrow  1','FontSize',10,'Color','b');
+	text(x_re2(IX2(21)),y_re2(IX2(21)),z_re2(IX2(21)),'\rightarrow 20','FontSize',10,'Color','b');
+	text(x_re2(IX2(31)),y_re2(IX2(31)),z_re2(IX2(31)),'\rightarrow 30','FontSize',10,'Color','b');
+	text(x_re2(IX2(41)),y_re2(IX2(41)),z_re2(IX2(41)),'\rightarrow 40','FontSize',10,'Color','b');
+	text(x_re2(IX2(51)),y_re2(IX2(51)),z_re2(IX2(51)),'\rightarrow 50','FontSize',10,'Color','b');
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+%	text(x_re3(IX3( 2)),y_re3(IX3( 2)),z_re3(IX3( 2)),'\rightarrow  1','FontSize',10,'Color','r');
+%	text(x_re3(IX3(21)),y_re3(IX3(21)),z_re3(IX3(21)),'\rightarrow 20','FontSize',10,'Color','r');
+%	text(x_re3(IX3(31)),y_re3(IX3(31)),z_re3(IX3(31)),'\rightarrow 30','FontSize',10,'Color','r');
+%	text(x_re3(IX3(41)),y_re3(IX3(41)),z_re3(IX3(41)),'\rightarrow 40','FontSize',10,'Color','r');
+%	text(x_re3(IX3(51)),y_re3(IX3(51)),z_re3(IX3(51)),'\rightarrow 50','FontSize',10,'Color','r');
 
-	
 	%% PLOTTING THE LEGEND!!!
-	scatter3(0, 0, 0, S(num_re1), 'k', 'filled');						% Training image re-plotted for visibility!
+	scatter3(0, 0, 0, bigScale, 'k', 'filled');						% Training image re-plotted for visibility!
 	legend(NomTI, NomAlg1, NomAlg2);				% Legend...
-	title(['Multi Resolution= ' int2str(current_resolution)]);				% etc...
+	title(['Multi Resolution = ' int2str(current_resolution)]);				% etc...
 	hold off; % end of plotting the 3-D graphic.
 	
 	
-	%%
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	
-	% X-Y Plot!!!
+
+	%% X-Y Plot!!!
 	figure('Name', 'X-Y Plot', 'NumberTitle', 'off');
 	scatter(x_re1, y_re1, S1, 'g', 'filled');
 	hold on;
 	scatter(x_re2, y_re2, S2, 'b', 'filled');
+% 	scatter(x_re3, y_re3, S3, 'r', 'filled');
+
+	% putting the labels to the special points
+	text(x_re1(IX1( 2)),y_re1(IX1( 2)),'\rightarrow  1','FontSize',10,'Color','g');
+	text(x_re1(IX1(21)),y_re1(IX1(21)),'\rightarrow 20','FontSize',10,'Color','g');
+	text(x_re1(IX1(31)),y_re1(IX1(31)),'\rightarrow 30','FontSize',10,'Color','g');
+	text(x_re1(IX1(41)),y_re1(IX1(41)),'\rightarrow 40','FontSize',10,'Color','g');
+	text(x_re1(IX1(51)),y_re1(IX1(51)),'\rightarrow 50','FontSize',10,'Color','g');
 	
-	% Code for the distinction for every i-th data plotted
-	S1(IX1( 2)) = mediumScale;			% the closest
-	S1(IX1( 4)) = mediumScale;			% the 3rd closest
-% 	S1(IX1(11)) = mediumScale;			% the 10th closest
-% 	S1(IX1(21)) = mediumScale;			% the 20th closest
-	S1(IX1(num_re1)) = mediumScale;	% the farthest
+	text(x_re2(IX2( 2)),y_re2(IX2( 2)),'\rightarrow  1','FontSize',10,'Color','b');
+	text(x_re2(IX2(21)),y_re2(IX2(21)),'\rightarrow 20','FontSize',10,'Color','b');
+	text(x_re2(IX2(31)),y_re2(IX2(31)),'\rightarrow 30','FontSize',10,'Color','b');
+	text(x_re2(IX2(41)),y_re2(IX2(41)),'\rightarrow 40','FontSize',10,'Color','b');
+	text(x_re2(IX2(51)),y_re2(IX2(51)),'\rightarrow 50','FontSize',10,'Color','b');
 	
-	text(x_re1(IX1( 2)), y_re1(IX1( 2)), '\rightarrow  1', 'FontSize', 10, 'Color', 'b');
-	text(x_re1(IX1( 4)), y_re1(IX1( 4)), '\rightarrow  3', 'FontSize', 10, 'Color', 'b');
-% 	text(x_re1(IX1(11)), y_re1(IX1(11)), '\rightarrow 10', 'FontSize', 10, 'Color', 'b');
-% 	text(x_re1(IX1(21)), y_re1(IX1(21)), '\rightarrow 20', 'FontSize', 10, 'Color', 'b');
-	text(x_re1(IX1(num_re1)), y_re1(IX1(num_re1)), ['\rightarrow ' num2str(num_re1)], 'FontSize', 10, 'Color', 'b');
+%	text(x_re3(IX3( 2)),y_re3(IX3( 2)),'\rightarrow  1','FontSize',10,'Color','r');
+%	text(x_re3(IX3(21)),y_re3(IX3(21)),'\rightarrow 20','FontSize',10,'Color','r');
+%	text(x_re3(IX3(31)),y_re3(IX3(31)),'\rightarrow 30','FontSize',10,'Color','r');
+%	text(x_re3(IX3(41)),y_re3(IX3(41)),'\rightarrow 40','FontSize',10,'Color','r');
+%	text(x_re3(IX3(51)),y_re3(IX3(51)),'\rightarrow 50','FontSize',10,'Color','r');
 	
-	
+	% ending of the plot
 	scatter(0, 0, 100, 'k', 'filled');
 	title(['x-y Plot & Multi Scale = ' int2str(current_resolution)]);
 	legend(NomAlg1, NomAlg2, NomTI);
 	hold off;
+		
 	
-	
-	
-	% X-Z Plot!!!
+	%% X-Z Plot!!!
 	figure('Name', 'X-Z Plot', 'NumberTitle', 'off');
 	scatter(x_re1, z_re1, S1, 'g', 'filled');
 	hold on;
 	scatter(x_re2, z_re2, S2, 'b', 'filled');
+% 	scatter(x_re3, z_re3, S3, 'r', 'filled');
 	
+	% putting the labels to the special points
+	text(x_re1(IX1( 2)),z_re1(IX1( 2)),'\rightarrow 1','FontSize',10,'Color','g');
+	text(x_re1(IX1(21)),z_re1(IX1(21)),'\rightarrow 20','FontSize',10,'Color','g');
+	text(x_re1(IX1(31)),z_re1(IX1(31)),'\rightarrow 30','FontSize',10,'Color','g');
+	text(x_re1(IX1(41)),z_re1(IX1(41)),'\rightarrow 40','FontSize',10,'Color','g');
+	text(x_re1(IX1(51)),z_re1(IX1(51)),'\rightarrow 50','FontSize',10,'Color','g');
+	
+	text(x_re2(IX2( 2)),z_re2(IX2( 2)),'\rightarrow 1','FontSize',10,'Color','b');
+	text(x_re2(IX2(21)),z_re2(IX2(21)),'\rightarrow 20','FontSize',10,'Color','b');
+	text(x_re2(IX2(31)),z_re2(IX2(31)),'\rightarrow 30','FontSize',10,'Color','b');
+	text(x_re2(IX2(41)),z_re2(IX2(41)),'\rightarrow 40','FontSize',10,'Color','b');
+	text(x_re2(IX2(51)),z_re2(IX2(51)),'\rightarrow 50','FontSize',10,'Color','b');
+	
+%	text(x_re3(IX3( 2)),z_re3(IX3( 2)),'\rightarrow 1','FontSize',10,'Color','r');
+%	text(x_re3(IX3(21)),z_re3(IX3(21)),'\rightarrow 20','FontSize',10,'Color','r');
+%	text(x_re3(IX3(31)),z_re3(IX3(31)),'\rightarrow 30','FontSize',10,'Color','r');
+%	text(x_re3(IX3(41)),z_re3(IX3(41)),'\rightarrow 40','FontSize',10,'Color','r');
+%	text(x_re3(IX3(51)),z_re3(IX3(51)),'\rightarrow 50','FontSize',10,'Color','r');
+	
+	% ending of the plot
 	scatter(0, 0, 100, 'k', 'filled');
 	title(['x-z Plot & Multi Scale = ' int2str(current_resolution)]);
 	legend(NomAlg1, NomAlg2, NomTI);
 	hold off;
 	
 	
-	
-	% Y-Z Plot!!!
+	%% Y-Z Plot!!!
 	figure('Name', 'Y-Z Plot', 'NumberTitle', 'off');
 	scatter(y_re1, z_re1, S1, 'g', 'filled');
 	hold on;
 	scatter(y_re2, z_re2, S2, 'b', 'filled');
+% 	scatter(y_re3, z_re3, S3, 'r', 'filled');
+
+	% putting the labels to the special points
+	text(y_re1(IX1( 2)),z_re1(IX1( 2)),'\rightarrow 1','FontSize',10,'Color','g');
+	text(y_re1(IX1(21)),z_re1(IX1(21)),'\rightarrow 20','FontSize',10,'Color','g');
+	text(y_re1(IX1(31)),z_re1(IX1(31)),'\rightarrow 30','FontSize',10,'Color','g');
+	text(y_re1(IX1(41)),z_re1(IX1(41)),'\rightarrow 40','FontSize',10,'Color','g');
+	text(y_re1(IX1(51)),z_re1(IX1(51)),'\rightarrow 50','FontSize',10,'Color','g');
 	
+	text(y_re2(IX2( 2)),z_re2(IX2( 2)),'\rightarrow 1','FontSize',10,'Color','b');
+	text(y_re2(IX2(21)),z_re2(IX2(21)),'\rightarrow 20','FontSize',10,'Color','b');
+	text(y_re2(IX2(31)),z_re2(IX2(31)),'\rightarrow 30','FontSize',10,'Color','b');
+	text(y_re2(IX2(41)),z_re2(IX2(41)),'\rightarrow 40','FontSize',10,'Color','b');
+	text(y_re2(IX2(51)),z_re2(IX2(51)),'\rightarrow 50','FontSize',10,'Color','b');
+	
+%	text(y_re3(IX3( 2)),z_re3(IX3( 2)),'\rightarrow 1','FontSize',10,'Color','r');
+%	text(y_re3(IX3(21)),z_re3(IX3(21)),'\rightarrow 20','FontSize',10,'Color','r');
+%	text(y_re3(IX3(31)),z_re3(IX3(31)),'\rightarrow 30','FontSize',10,'Color','r');
+%	text(y_re3(IX3(41)),z_re3(IX3(41)),'\rightarrow 40','FontSize',10,'Color','r');
+%	text(y_re3(IX3(51)),z_re3(IX3(51)),'\rightarrow 50','FontSize',10,'Color','r');
+	
+	% ending of the plot
 	scatter(0, 0, 100, 'k', 'filled');
 	title(['y-z Plot & Multi Scale = ' int2str(current_resolution)]);
 	legend(NomAlg1, NomAlg2, NomTI);
 	hold off;
 	
 	
-end	 % for ii=1:Pyramid
+end	 % for ii = 1:num_max_resolutions
 
 end
